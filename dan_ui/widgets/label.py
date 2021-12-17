97e1f205e8a01793aeb26a5da289121f960dc140
FosterToster <gribnoysalatik@gmail.com>
2021-12-18 00:30:19 +0700

    disable label animation

--- dan_ui/widgets/label.py
+++ dan_ui/widgets/label.py
@@ -7,17 +7,4 @@ class Label(Widget):
 
     def render(self, rect: DrawRect):
 
-        # if rect.life < 128:
-        #     y = round(rect.life * ((rect.size.y - 1) / 128))
-        # else:
-        #     y = rect.size.y - round((rect.life-128) * ((rect.size.y) / 128)) - 1
-
-        y = rect.life % rect.size.y
-        x = rect.life % (rect.size.x - len(self.text))
-
-        if y < 0:
-            y = 0
-        if y > rect.size.y - 1:
-            y = rect.size.y - 1
-
-        rect.textout(Point(x,y), self.text)
+        rect.textout(Point(0,0), self.text)
